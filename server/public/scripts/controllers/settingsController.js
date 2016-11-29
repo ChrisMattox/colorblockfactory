4b54b5cd202b35ac25d4cf28b4f1af63b4324933
Christopher Mattox <metroplex02@gmail.com>
2016-11-29 08:47:46 -0600

    working base mode

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -1,5 +1,14 @@
 colorBlocks.controller('SettingsController', ['$scope', 'DataFactory', function($scope, DataFactory) {
   console.log("settings controller running");
+  var self = this;
+  self.colors = DataFactory.colors;
+  self.newColor = "";
 
+  self.pushColorMaDoooods = function() {
+    self.colors.push(self.newColor);
+    self.newColor = "";
+    DataFactory.colors = self.colors;
+    console.log(self.colors);
+  }
 
 }]);
